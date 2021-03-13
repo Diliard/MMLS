4d167a8e0e73caa350c80984a9762422c46c1d7a
Diliard <43886528+Diliard@users.noreply.github.com>
2021-03-13 03:08:05 +0100

    fixed the config and it's usage
    config now uses the entity types directly no string's and the actual check's work properly too (except leashes) need to look into it further
    
    also fixed my dumb commit from before might actually figure out how to git soonTM

--- src/main/java/mod/marcymooslaggyslaggy/mixin/ServerWorldMixin.java
+++ src/main/java/mod/marcymooslaggyslaggy/mixin/ServerWorldMixin.java
@@ -1,5 +1,6 @@
 package mod.marcymooslaggyslaggy.mixin;
 
+import mod.marcymooslaggyslaggy.config.Config;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.TameableEntity;
@@ -27,6 +28,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 @Mixin(ServerWorld.class)
 abstract
@@ -165,7 +167,7 @@ class Clear {
         }
 
         //1 Hour
-        if (secondsPassed == 3600) {
+        if (secondsPassed == 10) {
             tick = 0;
             secondsPassed = 0;
             EntPurge();
@@ -177,11 +179,14 @@ class Clear {
     private void EntPurge() {
         ServerWorld instance = (ServerWorld) (Object) this;
         for (Entity entity : instance.getEntitiesByType(null, EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR)) {
+            /*
 
             if (entity instanceof TameableEntity) {
                 TameableEntity tameableEntity = (TameableEntity) entity;
 
-                if (tameableEntity.isTamed() || tameableEntity.hasCustomName() || tameableEntity.hasVehicle()) {
+                if (tameableEntity.isTamed() || tameableEntity.hasCustomName() ||
+                        tameableEntity.hasVehicle() || entity.getType() == EntityType.BEE ||
+                        entity.getType() == EntityType.HORSE || entity.getType() == EntityType.SKELETON_HORSE || entity.getType() == EntityType.DONKEY) {
                 } else {
                     entity.kill();
                 }
@@ -195,12 +200,17 @@ class Clear {
                     entity.getType() == EntityType.TNT_MINECART || entity.getType() == EntityType.ELDER_GUARDIAN ||
                     entity.getType() == EntityType.ENDER_DRAGON || entity.getType() == EntityType.WITHER || entity.getType() == EntityType.SHULKER ||
                     entity.getType() == EntityType.VILLAGER || entity.hasVehicle() || entity.getType() == EntityType.BOAT || entity.getType() == EntityType.LEASH_KNOT ||
+                    entity.getType() == EntityType.BEE || entity.getType() == EntityType.HORSE || entity.getType() == EntityType.SKELETON_HORSE || entity.getType() == EntityType.DONKEY ||
+                    entity.getType() == EntityType.EVOKER ||
                     entity instanceof PlayerEntity || entity instanceof TameableEntity) {
+            */
 
+            if (Config.WhitelistedEntities.contains(entity.getType())  || entity.hasVehicle() || entity.hasCustomName() || entity instanceof PlayerEntity) { }
+            else if (entity instanceof TameableEntity) {if (((TameableEntity) entity).isTamed() || ((TameableEntity) entity).isLeashed()) {}}
+            else if (entity.hasCustomName() || entity.hasVehicle()){
             } else {
                 entity.kill();
             }
-
         } EntPurge2();
     }
     private void EntPurge2() {
